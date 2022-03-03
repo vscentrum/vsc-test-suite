aac1321afa4c47632bddedc0566b95048ffe8d9a
Michele Pugno <michele.pugno@gmail.com>
2022-03-03 13:47:34 +0100

    added extra filter to version check, reframe 3.10.1 antwerp

--- tests/cue/src/version_check.py
+++ tests/cue/src/version_check.py
@@ -11,13 +11,13 @@ out = subprocess.run([cmd],shell=True, stdout=subprocess.PIPE)
 out = out.stdout.decode('utf-8')
 
 if re.search(r'not installed', out):
-    cmd = f"{args[1]} --version | sed 1q"
+    cmd = f"{args[1]} --version | sed '/^$/d' | sed 1q"
     out = subprocess.run([cmd],shell=True, stdout=subprocess.PIPE)
     out = out.stdout.decode('utf-8')
 
 with open('data', 'w') as file:
     file.write(out)
-    
+
 out = subprocess.run(['sed', r's/^.*[^0-9]\([0-9]*\.[0-9]*\.[0-9]*\).*$/\1/', 'data'], stdout=subprocess.PIPE)
 out = out.stdout.decode('utf-8')
 print(version(args[2]) <= version(out))  # True
