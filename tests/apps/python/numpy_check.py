493ab25e4386cc5ffafca018e5cdab48b1d44a4b
Michele Pugno <michele.pugno@gmail.com>
2022-03-14 11:22:34 +0100

    remove old license

--- tests/apps/python/numpy_check.py
+++ tests/apps/python/numpy_check.py
@@ -1,8 +1,3 @@
-# Copyright 2016-2021 Swiss National Supercomputing Centre (CSCS/ETH Zurich)
-# ReFrame Project Developers. See the top-level LICENSE file for details.
-#
-# SPDX-License-Identifier: BSD-3-Clause
-
 import reframe as rfm
 from reframe.core.decorators import run_after
 import reframe.utility.sanity as sn
