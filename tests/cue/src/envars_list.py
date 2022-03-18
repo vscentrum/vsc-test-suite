1a9f776a2ea2375f9c2fc1dbe7668838564b2e81
Samuel Moors <samuel.moors@vub.be>
2022-03-18 08:12:13 +0100

    relax arch name spec

--- tests/cue/src/envars_list.py
+++ tests/cue/src/envars_list.py
@@ -2,17 +2,17 @@ envars = {
     'VSC_HOME':
     {
         'name': 'VSC_HOME',
-        'exe': ["""print(os.environ["USER"] in os.environ["{}"])"""],
+        'exe': ['print(os.environ["USER"] in os.environ["{}"])'],
     },
     'VSC_DATA':
     {
         'name': 'VSC_DATA',
-        'exe': ["""print(os.environ["USER"] in os.environ["{}"])"""],
+        'exe': ['print(os.environ["USER"] in os.environ["{}"])'],
     },
     'VSC_SCRATCH':
     {
         'name': 'VSC_SCRATCH',
-        'exe': ["""print(os.environ["USER"] in os.environ["{}"])"""],
+        'exe': ['print(os.environ["USER"] in os.environ["{}"])'],
     },
     'VSC_ARCH_LOCAL':
     {
@@ -20,23 +20,25 @@ envars = {
         # archspec is automatically installed as part of ReFrame
         'exe': [
             'import archspec.cpu',
-            """print(archspec.cpu.host().name.split("_")[0] == os.environ["{}"])""",
+            'arch = archspec.cpu.host().name',
+            'archlist = [arch, arch.split("_")[0]]',  # relaxed spec, allows both skylake and skylake_avx512
+            'print(os.environ["{}"] in archlist)',
         ],
     },
     'VSC_ARCH_SUFFIX':
     {
         'name': 'VSC_ARCH_SUFFIX',
-        'exe': ["""print(os.environ["{}"] in ["", "-ib"])"""],
+        'exe': ['print(os.environ["{}"] in ["", "-ib"])'],
     },
     'VSC_INSTITUTE':
     {
         'name': 'VSC_INSTITUTE',
-        'exe': ["""print(os.environ["{}"] in ["antwerpen", "brussel", "gent", "leuven"])"""],
+        'exe': ['print(os.environ["{}"] in ["antwerpen", "brussel", "gent", "leuven"])'],
     },
     'VSC_INSTITUTE_LOCAL':
     {
         'name': 'VSC_INSTITUTE_LOCAL',
-        'exe': ["""print(os.environ["{}"] in ["antwerpen", "brussel", "gent", "leuven"])"""],
+        'exe': ['print(os.environ["{}"] in ["antwerpen", "brussel", "gent", "leuven"])'],
     },
     'VSC_INSTITUTE_CLUSTER':
     {
@@ -45,16 +47,16 @@ envars = {
     'VSC_OS_LOCAL':
     {
         'name': 'VSC_OS_LOCAL',
-        'exe': ["""print(os.environ["{}"] in ["CO7", "RHEL8", "centos8", "centos7"])"""],
+        'exe': ['print(os.environ["{}"] in ["CO7", "RHEL8", "centos8", "centos7"])'],
     },
     'VSC_SCRATCH_NODE':
     {
         'name': 'VSC_SCRATCH_NODE',
-        'exe': ["""print(os.environ["{}"] in ["/local", "/node_scratch", "/tmp"])"""],
+        'exe': ['print(os.environ["{}"] in ["/local", "/node_scratch", "/tmp"])'],
     },
     'VSC_SCRATCH_SITE':
     {
         'name': 'VSC_SCRATCH_SITE',
-        'exe': ["""print(os.environ["{}"] == os.environ["VSC_SCRATCH"])"""],
+        'exe': ['print(os.environ["{}"] == os.environ["VSC_SCRATCH"])'],
     },
 }
