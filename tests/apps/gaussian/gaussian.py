e8e99bac5034d6c48ef65655eb03fe42fce1ac70
Michele Pugno <michele.pugno@gmail.com>
2022-06-23 18:38:55 +0200

    added Gaussian and Matlab Test for uantwerp

---
+++ tests/apps/gaussian/gaussian.py
@@ -0,0 +1,59 @@
+
+import reframe as rfm
+import reframe.utility.sanity as sn
+from reframe.core.backends import getlauncher
+import os
+
+
+class GaussianBaseTest(rfm.RunOnlyRegressionTest):
+    def __init__(self):
+        self.valid_prog_environs = ['builtin']
+        self.modules = ['Gaussian/g16_c01-avx2']
+
+        self.sanity_patterns = sn.assert_found(r' Normal termination of Gaussian',
+                                               self.stdout)
+        self.perf_patterns = {
+            'time': (
+                sn.extractsingle(
+                r'^real\t(?P<minutes>\S+)m\S+s',
+                'rfm_GaussianCPUTest_job.err', 'minutes', float) + 
+                sn.extractsingle(
+                r'^real\t\S+m(?P<seconds>\S+)s',
+                'rfm_GaussianCPUTest_job.err', 'seconds', float) / 60.0)
+        }
+
+        self.maintainers = ['Lewih']
+
+
+@rfm.simple_test
+class GaussianCPUTest(GaussianBaseTest):
+    def __init__(self):
+        super().__init__()
+        self.valid_systems = ['leibniz:single-node', 'vaughan:single-node']
+        self.reference = {
+            'leibniz:single-node': {
+                'time': (33.0, -0.5, 0.5, 'minutes/percentageOfMinute'),
+
+            },
+            'vaughan:single-node': {
+                'time': (15.0, -0.05, 0.05, 'minutes/percentageOfMinute'),
+                },
+        }
+        self.tags = {'apps', 'gaussian', 'performance'}
+
+
+    @run_after('setup')
+    def set_num_cpus(self):
+        if self.current_system.name == 'leibniz':
+    
+            self.num_cpus_per_task = 28
+        else:
+            self.num_cpus_per_task = 64
+
+        self.executable = f'time g16 < mich-{self.current_system.name}.com'
+    
+        self.descr = f'Single Node Gaussian Test, cpus{self.num_cpus_per_task}'
+
+    @run_before('run')
+    def replace_launcher(self):
+        self.job.launcher = getlauncher('local')()
