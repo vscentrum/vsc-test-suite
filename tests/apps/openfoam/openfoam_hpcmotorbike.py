e117a619b265d8899502af43a99bde4174630545
Michele Pugno <michele.pugno@gmail.com>
2022-07-06 18:17:49 +0200

    added openfoam tag

--- tests/apps/openfoam/openfoam_hpcmotorbike.py
+++ tests/apps/openfoam/openfoam_hpcmotorbike.py
@@ -120,7 +120,7 @@ class OpenFOAMHPCMotorbikeTest(rfm.RunOnlyRegressionTest):
     maintainers = ['stevenvdb']
     time_limit = '12h'
     executable = './run_openfoam_hpc_benchmark.sh'
-    tags = {"vsc", "apps", "performance", "resource-intensive"}
+    tags = {"vsc", "apps", "performance", "resource-intensive", "openfoam"}
     sourcesdir = 'sources'
     keep_files = ['log.simpleFoam', 'log.checkMesh']
 
